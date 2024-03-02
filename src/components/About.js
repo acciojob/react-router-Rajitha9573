60093670d513e80018e0eebed29cf73037d2239b
Rajitha9573 <rajithabhukya967@gmail.com>
2024-03-02 15:25:41 +0530

    react-router

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+const About = ()=>{
+
+
+    return (
+
+        <div>
+
+            <p>This is a sample React Router Program</p>
+
+
+        </div>
+    )
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 
 import React from "react";
-import './../styles/App.css';
+import './../styles/App.css'
+import Home from "./Home";
+import About from "./About";
+import Nav from "./Nav";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
+      
+      <BrowserRouter>
+      <Nav />
+
+      <Routes>
+        <Route path ="/" element={ <Home />}/>
+        <Route path ="/about" element={ <About />}/>
+
+
+      </Routes>
+      
+      </BrowserRouter>
+      
+     
+     
+
         {/* Do not remove the main div */}
     </div>
   )
